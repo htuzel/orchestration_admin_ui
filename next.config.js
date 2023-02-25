a3ed27774b8cbdc7a126c468c01f23c7409bf6aa
enginesa <engin1218@hotmail.com>
2023-02-25 13:12:16 +0300

    login

--- next.config.js
+++ next.config.js
@@ -2,7 +2,6 @@
 const path = require('path')
 
 const nextConfig = {
-  reactStrictMode: true,
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
--- src/pages/login.js
+++ src/pages/login.js
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Button from '@mui/material/Button';
+import styles from '@/styles/Login.module.scss';
+import TextField from '@mui/material/TextField';
 
 const Login = () => {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    console.log(username);
+    console.log(password);
     return (
-        <div>
-           <h1>Login</h1>
+        <div className={styles.login}>
+            <div className="content">
+                <div className="logo"><img src="https://flalingo.com/cdn-cgi/image/f=auto,width=550,quality=90/img/new_home/responsive_img/logo_withouth_motto_w_1400.webp"/></div>
+                <div className="form">
+                    <TextField required label="Username" onChange={(e)=>{setUsername(e.target.value)}}/>
+                    <TextField required label="Password" onChange={(e)=>{setPassword(e.target.value)}}/>
+                    <Button variant="contained" size={"large"}>Login</Button>
+                    <p className="forgot-password">Forgot Password?</p>
+
+                </div>
+                <div className="other"></div>
+                <div className="no-account">Don't have an account? <a href="#">Sign Up</a></div>
+            </div>
         </div>
     );
 };
