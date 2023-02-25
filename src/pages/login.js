22e377696220794d91852d91a83b37a01f63a68c
enginesa <engin1218@hotmail.com>
2023-02-25 13:14:46 +0300

    Update login.js

--- src/pages/login.js
+++ src/pages/login.js
@@ -7,18 +7,23 @@ const Login = () => {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
 
-    console.log(username);
-    console.log(password);
+    const handleLogin = () => {
+        
+    }
+
     return (
         <div className={styles.login}>
             <div className="content">
                 <div className="logo"><img src="https://flalingo.com/cdn-cgi/image/f=auto,width=550,quality=90/img/new_home/responsive_img/logo_withouth_motto_w_1400.webp"/></div>
                 <div className="form">
-                    <TextField required label="Username" onChange={(e)=>{setUsername(e.target.value)}}/>
-                    <TextField required label="Password" onChange={(e)=>{setPassword(e.target.value)}}/>
-                    <Button variant="contained" size={"large"}>Login</Button>
+                    <TextField required label="Username" onChange={(e) => {
+                        setUsername(e.target.value)
+                    }}/>
+                    <TextField required label="Password" type={"password"} onChange={(e) => {
+                        setPassword(e.target.value)
+                    }}/>
+                    <Button variant="contained" size={"large"} onClick={() => handleLogin()}>Login</Button>
                     <p className="forgot-password">Forgot Password?</p>
-
                 </div>
                 <div className="other"></div>
                 <div className="no-account">Don't have an account? <a href="#">Sign Up</a></div>
